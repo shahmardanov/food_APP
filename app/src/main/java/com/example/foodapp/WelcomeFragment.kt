c186f43e2207bc409e0df55c43b9d75587d88f64
shamerdanov <shamil332211@list.ru>
2024-05-30 14:48:46 +0400

    add ui components to welcomefragment

---
+++ app/src/main/java/com/example/foodapp/WelcomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.foodapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class WelcomeFragment : Fragment() {
+
+
+}
